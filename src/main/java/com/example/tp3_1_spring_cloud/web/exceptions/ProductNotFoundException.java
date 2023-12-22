7fee933c18a19bb5587cccec17ad4213e7c1cac6
saadalami66 <104860158+saadalami66@users.noreply.github.com>
2023-12-22 23:05:37 +0100

    Delete src directory

--- src/main/java/com/example/tp3_1_spring_cloud/web/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.tp3_1_spring_cloud.web.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
